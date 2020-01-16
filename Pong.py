92f6a83753865ffc2ed777de4325ca277e2d34ad
laan71 <lail5198@edu.easj.dk>
2020-01-17 00:04:29 +0100

    Still working on the speed of ball and paddles and restrict the paddles to the board (not shown in the tutorial)

---
+++ Pong.py
@@ -0,0 +1,142 @@
+import turtle
+import os
+import winsound # for windows
+
+wn = turtle.Screen()
+wn.title("Pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Score
+score_a = 0
+score_b = 0
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 1
+ball.dy = -1
+
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+
+# Function
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 40
+    paddle_a.sety(y)
+
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 40
+    paddle_a.sety(y)
+
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 30
+    paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 30
+    paddle_b.sety(y)
+
+
+# Keyboard binding
+wn.listen()
+wn.onkey(paddle_a_up, "w")
+wn.onkey(paddle_a_down, "s")
+wn.onkey(paddle_b_up, "Up")
+wn.onkey(paddle_b_down, "Down")
+
+
+# Main game loop
+while True:
+    wn.update()
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+        # os.system("afplay Bounce.wav&") # Mac OS
+        # os.system("aplay Bounce.wav&") # linux OS
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC) # Windows OS
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+        # os.system("afplay Bounce.wav&") # Mac OS
+        # os.system("aplay Bounce.wav&") # linux OS
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC) # Windows OS
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        # os.system("afplay Bounce.wav&") # Mac OS
+        # os.system("aplay Bounce.wav&") # linux OS
+        winsound.PlaySound("crash.wav", winsound.SND_ASYNC) # Windows OS
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        # os.system("afplay Bounce.wav&") # Mac OS
+        # os.system("aplay Bounce.wav&") # linux OS
+        winsound.PlaySound("crash.wav", winsound.SND_ASYNC) # Windows OS
+
+    # Paddle and Ball collisions
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1
+        # os.system("afplay Bounce.wav&") # Mac OS
+        # os.system("aplay Bounce.wav&") # linux OS
+        winsound.PlaySound("pongblipe1.wav", winsound.SND_ASYNC) # Windows OS
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *= -1
+        # os.system("afplay Bounce.wav&") # Mac OS
+        # os.system("aplay Bounce.wav&") # linux OS
+        winsound.PlaySound("pongblipe2.wav", winsound.SND_ASYNC) # Windows OS
